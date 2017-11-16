13326158f15e60809fa9d666a65300904e4387a0
Joe Eappen <ee13b080@ee.iitm.ac.in>
2017-11-17 00:14:48 +0530

    got env working. now to actually code up some stuff

--- rldt/envs/env.py
+++ rldt/envs/env.py
@@ -2,20 +2,30 @@
 
 import numpy as np
 import gym
+from gym import error, spaces, utils # need this!
+
+high = 10
+QUERY0 = 0
+QUERY1 = 1
+QUERY2 = 2
+PREDICT0 = 3 
+PREDICT1 = 4
 
 
 class DTLearner(gym.Env): 
-    def __init__(self):
+    def __init__(self, os_size = None):
+        if os_size is None:
+            os_size = 1
         self._reset()
         self.action_space = spaces.Discrete(6)
-        self.observation_space = spaces.Box(-high, high) #INCOMPLETE
+        self.observation_space = spaces.Box(np.zeros(os_size) - high, np.zeros(os_size)+ high) #INCOMPLETE
         self.done = False
         self.state = None
 
     def _step(self,action):
         assert self.action_space.contains(action)
         state = self.state
-        
+        val = 0
         if action == QUERY0:
             val = data[0]
         elif action == QUERY1:
@@ -45,5 +55,5 @@ class DTLearner(gym.Env):
         self.state += [val]
         return self.state,reward, done, dummy
 
-        def _reset(self):
-            pass 
+    def _reset(self):
+        pass 
